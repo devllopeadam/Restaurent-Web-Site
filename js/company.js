7b32719a47e06adc660c5149c425bac14dd7516b
devllopeadam <jeniahadam@gmail.com>
2023-12-15 23:13:18 +0100

    other submit

--- js/company.js
+++ js/company.js
@@ -1,26 +1,3 @@
 
 const headerSection = document.querySelector(".header-section");
 window.onload = () => headerSection.classList.add("animation");
-
-// const ctaImage = document.querySelector(".cta-full-image");
-// const values = ctaImage.querySelectorAll("h1");
-// let started = false;
-
-// function startCount(el) {
-//   let value = el.dataset.value;
-//   let count = setInterval(() => {
-//     el.textContent++;
-//     if (el.textContent === value) {
-//       clearInterval(count);
-//     }
-//   }, 1500 / value)
-// }
-
-// window.onscroll = () => {
-  // if (window.scrollY >= ctaImage.offsetTop) {
-  //   if (!started) {
-  //     values.forEach(el => startCount(el))
-  //   }
-  //   started = true;
-  // }
-// }
